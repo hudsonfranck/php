2c33ed8fa05803387401ed9e9631ce5c4917c786
Hudson <hudson_franck@yahoo.com.br>
2023-12-07 14:12:41 -0300

    'mensagem'

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
-<<<<<<< HEAD
- 	
  	echo "Wellcome to PHP";
- echo "Traduzir o texto";
+	echo "Traduzir o texto";
  	echo "2023"
 
 ?>
-=======
-
-echo "Ola mundo PHP 789!";
-echo "teste de pull"
-echo "ola udemy"
-
-?>
->>>>>>> bf1ad63e08a75fd4d62eb3e6f5e2b2e335873944
